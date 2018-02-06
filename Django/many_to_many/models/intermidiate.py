2fca7add619434109ec746383966220fc23a0583
Seungri Cho <pseudo.circuit@gmail.com>
2018-02-06 10:56:19 +0900

    many-to-many package 화

--- Django/many_to_many/models/intermidiate.py
+++ Django/many_to_many/models/intermidiate.py
@@ -4,22 +4,14 @@ from django.db import models
 from django.utils import timezone
 
 
-class Topping(models.Model):
-    name = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.name
-
-
-class Pizza(models.Model):
-    name = models.CharField(max_length=50)
-    toppings = models.ManyToManyField(Topping)
-
-    def __str__(self):
-        return self.name
+__all__ = (
+    'Post',
+    'User',
+    'PostLike',
+)
 
 
-# Extra fields on many-to-many relationships
 class Post(models.Model):
     title = models.CharField(max_length=50)
     like_users = models.ManyToManyField(
