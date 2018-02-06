e2d7751bac32ab3528261bb1fc755b161e580740
Seungri Cho <pseudo.circuit@gmail.com>
2018-02-06 14:44:37 +0900

    follow, block. following block 구현

--- Django/many_to_many/models/symmetrical_intermediate.py
+++ Django/many_to_many/models/symmetrical_intermediate.py
@@ -25,9 +25,21 @@ class TwitterUser(models.Model):
         related_name='+',
     )
 
+
     def __str__(self):
         return self.name
 
+    @property
+    def block_users(self):
+        """
+        내가 block 하고 있는 TwitterUser 목록
+        :return:
+        """
+        pk_list = self.relations_by_from_user.filter(
+            type=Relation.RELATION_TYPE_BLOCK).values_list('to_user', flat=True)
+        return TwitterUser.objects.filter(pk__in=pk_list)
+
+    @property
     def following(self):
         """
         내가 folow하고 있는 TwitterUser 목록을 가져옴
@@ -44,6 +56,26 @@ class TwitterUser(models.Model):
         #   에 포함되는 User목록을 following_users변수로 할당
         following_users = TwitterUser.objects.filter(pk__in=following_pk_list)
         return following_users
+
+    def follow(self, to_user):
+        """
+        to_user에 주어진 TwitterUser를 followgka
+        :param to_user:
+        :return:
+        """
+        self.relations_by_from_user.filter(to_user=to_user).delete()
+        self.relations_by_from_user.create(
+            to_user=to_user,
+            type=Relation.RELATION_TYPE_FOLLOWING,
+        )
+
+    def block(self, to_user):
+        self.relations_by_from_user.filter(to_user=to_user).delete()
+        self.relations_by_from_user.create(
+            to_user=to_user,
+            type=Relation.RELATION_TYPE_BLOCK,
+        )
+
 
 class Relation(models.Model):
     """
@@ -70,3 +102,14 @@ class Relation(models.Model):
         related_name='relations_by_to_user',
     )
     type = models.CharField(max_length=1, choices=CHOICES_TYPE)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    # class 전체에 적용
+    class Meta:
+        unique_together = (
+            # from_user와 to_user의 값이 이미 있을 경우
+            # DB에 중복 데이터 저장을 막음
+            # ex) from_user가 1, to_user가 3인 데이터가 이미 있다면
+            #   두 항목의 값이 모두 같은 또 다른 데이터가 존재할 수 없음
+           ('from_user', 'to_user'),
+        )
