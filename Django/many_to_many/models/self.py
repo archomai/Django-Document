eceebfd996ffef5b7d4f67f04e621ce90b86e529
Seungri Cho <pseudo.circuit@gmail.com>
2018-02-06 11:37:46 +0900

    Facebook_user 출력

--- Django/many_to_many/models/self.py
+++ Django/many_to_many/models/self.py
@@ -9,4 +9,18 @@ class FacebookUser(models.Model):
     friends = models.ManyToManyField('self')
 
     def __str__(self):
-        return self.name
+        # name 이 '이한영'이며
+        # 친구로 '박보영', '아이유'를 가지는 경우
+        # -> 이한영 (친구 : 박보영, 아이유)
+        # __str__의 결과가 위처름 출력될 수 있도록 작성
+
+        # list comprehension 사용
+        # friends_string = ', '.join([f.name for f in self.friends.all()])
+
+        # Manager의 values_list를 사용
+        friends_string = ', '.join(self.friends.value_list('name', flat=True))
+
+        return '{name} (친구 : {friends})'.format(
+            name=self.name,
+            friends=friends_string,
+        )
