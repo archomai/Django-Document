5edd85f67f6b825461ceadee76a8aedef983f8c6
Seungri Cho <pseudo.circuit@gmail.com>
2018-02-08 14:46:02 +0900

    inheritance.abstract_base_classes 추가

--- Django/one_to_one/models.py
+++ Django/one_to_one/models.py
@@ -6,7 +6,7 @@ class Place(models.Model):
     address = models.CharField(max_length=80)
 
     def __str__(self):
-        return "%s the place" % self.name
+        return f'{self.name} the place'
 
 
 class Restaurant(models.Model):
@@ -19,7 +19,7 @@ class Restaurant(models.Model):
     serves_pizza = models.BooleanField(default=False)
 
     def __str__(self):
-        return "%s the restaurant" % self.place.name
+        return f'{self.place.name} the restaurant'
 
 
 class Waiter(models.Model):
@@ -27,4 +27,4 @@ class Waiter(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return "%s the waiter at %s" % (self.name, self.restaurant)
+        return f'{self.name} the waiter at {self.restaurant}'
