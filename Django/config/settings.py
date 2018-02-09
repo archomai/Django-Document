3a6b98127aa6ca0c54ad380b4522761a89baccb5
Seungri Cho <pseudo.circuit@gmail.com>
2018-02-09 12:24:59 +0900

    inheritance.multi_table 모델 models.py 작성

--- Django/config/settings.py
+++ Django/config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
+
     'myapp',
 
     'relations.foreignkey.apps.ForeignkeyConfig',
--- Django/inheritance/multi_table/models.py
+++ Django/inheritance/multi_table/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Place(models.Model):
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=80)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=80, blank=True, null=True)
 
     def __str__(self):
         return f'{self.name} | {self.address}'
 
 
-class Restaurant(models.Model):
+class Restaurant(Place):
     serves_hot_dogs = models.BooleanField(default=False)
     serves_pizza = models.BooleanField(default=False)
 
