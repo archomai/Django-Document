05b4e8754f991523378f101a412cdc32201b3ee5
Seungri Cho <pseudo.circuit@gmail.com>
2018-02-19 12:53:58 +0900

    postgresql 변경

--- app/weblog/models.py
+++ app/weblog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.db import models
 
+
 class Blog(models.Model):
     name = models.CharField(max_length=100)
     tagline = models.TextField(blank=True)
@@ -9,12 +10,14 @@ class Blog(models.Model):
     def __str__(self):
         return self.name
 
+
 class Author(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(blank=True)
 
     def __str__(self):
         return self.name
+
 
 class Entry(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
